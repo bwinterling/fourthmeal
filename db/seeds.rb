c1890e78cfe67d8789679ef1ce66546ff4c6bd30
Your Name <you@domain.com>
2013-11-05 12:15:27 -0700

    Create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,123 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Item.create!([
+  {
+    title: "Burrito",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "BURRITO.png"
+  },
+  {
+    title: "Breakfast",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "BREAKFAST.png"
+  },
+  {
+    title: "3 Cheese Nachos",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "3_CHEESE_NACHOS.png"
+  },
+  {
+    title: "Quesadilla",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "Quesadilla.png"
+  },
+  {
+    title: "Tacos",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "TACOS.png"
+  },
+  {
+    title: "Taco Salad",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "TACO_SALAD.png"
+  },
+  {
+    title: "Naked Burrito",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "NAKED_BURRITO.png"
+  },
+  {
+    title: "Chips Dip",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "CHIPS_DIP.png"
+  },
+  {
+    title: "Signature Gumbo",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "SIG_GUMBO.png"
+  },
+  {
+    title: "Signature Queso Burrito",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "SIG_QUESO_BURRITO.png"
+  },
+  {
+    title: "Kids Taco",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "KIDS_TACO.png"
+  },
+  {
+    title: "Signature Fajita Burrito",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "SIG_FAJITA_BURRITO.png"
+  },
+  {
+    title: "Craft 2 Tortilla Soup",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "C2_TACO_TORT_SOUP.png"
+  },
+  {
+    title: "Kids Quesadilla",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "KIDS_QUESADILLA.png"
+  },
+  {
+    title: "Signature Veggie Burrito",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "SIG_VEGGIE_BURRITO.png"
+  },
+  {
+    title: "Kids Naked Burrito",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "KIDS_NAKED_BURRITO.png"
+  },
+  {
+    title: "Kids Meal",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "KIDS_LEADING.png"
+  },
+  {
+    title: "Craft 2 Gumbo",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "C2_TACO_GUMBO.png"
+  },
+  {
+    title: "Craft 2 Nacho",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "C2_NACHO_DILLA.png"
+  },
+  {
+    title: "Craft 2 Taco",
+    description: "beans and rice and cheese",
+    price: 7.00,
+    photo: "C2_DILLA_NAKED_TACO.png"
+  },
+])
+
