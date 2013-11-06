ce4cf67ec8e8c18580ab45ecf056a9cd5f1dc84a
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-11-05 17:25:26 -0700

    Validations for item categories

--- app/models/item_category.rb
+++ app/models/item_category.rb
@@ -1,2 +1,6 @@
 class ItemCategory < ActiveRecord::Base
+  validates :item_id, presence: true
+  validates :category_id, presence: true
+  validates_numericality_of :item_id
+  validates_numericality_of :category_id
 end
--- test/models/item_category_test.rb
+++ test/models/item_category_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ItemCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "it_validates_status" do
+    @item_category = ItemCategory.create()
+    assert @item_category.invalid?
+  end
+
+  test "it_validates_item_id" do
+    @item_category = ItemCategory.create(:item_id => "")
+    assert @item_category.invalid?
+  end
+
+  test "it_validates_category_id" do
+    @item_category = ItemCategory.create(:item_id => "5", category_id: "")
+    assert @item_category.invalid?
+  end
+
 end
