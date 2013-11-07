f7386b74d0ef5e410b74ddd434a0edc250c32caa
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-11-07 11:15:09 -0700

    Added Order Now feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def navbar_order_link
+    if current_order.id
+      link_to "Your Order", order_path(current_order)
+    else
+      link_to "Order Now!", items_path
+    end
+  end
 end
