71b4893df73d11b833763cc5bc17152ec78d173b
Ben Lewis <bennlewis@gmail.com>
2013-11-07 14:03:35 -0700

    updated seeds to include images and titles, added image links, added cart functionality, and bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -17,14 +17,14 @@ ActiveRecord::Schema.define(version: 20131106155450) do
   enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
-    t.string   "title",      null: false
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "item_categories", force: true do |t|
-    t.integer  "item_id",     null: false
-    t.integer  "category_id", null: false
+    t.integer  "item_id"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20131106155450) do
   create_table "items", force: true do |t|
     t.text     "title"
     t.string   "description"
-    t.decimal  "price",       precision: 10, scale: 2
+    t.decimal  "price"
     t.string   "photo"
     t.datetime "created_at"
     t.datetime "updated_at"
