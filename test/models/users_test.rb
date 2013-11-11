c26e33623e076df681a1191a4bb58382ecf39f46
Tyler Long <tyler.stephen.long@gmail.com>
2013-11-11 16:03:22 -0700

    Update Users Tests

--- test/models/users_test.rb
+++ test/models/users_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class UsersTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "it_validates_its_attributes" do
+    @user = User.new
+    @user.save
+    assert @user.invalid?
+  end
+
+  test "it_invalid_email" do
+    @user = User.create(:email => "Smith@example.com")
+    assert @user.invalid?
+  end
+
+ 
+  test "it_validates_full_name" do
+    @user = User.create(:full_name => "George Branson")
+    assert @user.invalid?
+  end
+
+  test "it_validates_display_name" do 
+    @user = User.create(:display_name => "Hashrocket")
+    assert @user.invalid?
+  end
+ 
 end
