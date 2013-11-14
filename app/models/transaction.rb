004242ad3326428accff8a5e62f813f070ab54af
Tyler Long <tyler.stephen.long@gmail.com>
2013-11-13 20:52:32 -0700

    update transaction validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,7 +4,7 @@ class Transaction < ActiveRecord::Base
   validates_presence_of :first_name 
   validates_presence_of :last_name
   validates_format_of   :credit_card_number, with: /\d{16}/
-  validates_format_of   :credit_card_expiration, with: /\d{4}/
+  validates_format_of   :credit_card_expiration, with: /\d{2}\/\d{2}/, :message => "Expiration Date should be entered as mm/yy "
   validates_format_of   :zipcode, with: /\d{5}/
 
   def pay!
