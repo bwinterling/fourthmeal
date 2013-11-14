cc8eab4aaf5c69d590cc2da834b6f3b03fea7717
Ben Lewis <bennlewis@gmail.com>
2013-11-13 19:01:32 -0700

    removed dead ref to item.css, commented out create in items controlelr

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,32 +17,31 @@ class ItemsController < ApplicationController
     render :index
   end
 
-  def new
-    @item = Item.new
-  end
+  # def new
+  #   @item = Item.new
+  # end
 
-  def create
-    @item = Item.new(
-      :title => params[:item][:title],
-      :description => params[:item][:description],
-      :price => params[:item][:price].to_f)
+  # def create
+  #   @item = Item.new(
+  #     :title => params[:item][:title],
+  #     :description => params[:item][:description],
+  #     :price => params[:item][:price].to_f)
 
-    if @item.save!
-      flash.notice = "#{@item.title} was created"
-      redirect_to items_path
-    else
-      flash.notice = "Sorry, there was a problem with the item you tried to create.
-      Please check your input and try again."
-      render :new
-    end
-  end
+  #   if @item.save!
+  #     flash.notice = "#{@item.title} was created"
+  #     redirect_to items_path
+  #   else
+  #     flash.notice = "Sorry, there was a problem with the item you tried to create.
+  #     Please check your input and try again."
+  #     render :new
+  #   end
+  # end
 
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
     flash.notice = "#{@item.name} was updated"
     redirect_to edit_item_path(@item.id)
-    #redirect_to admin_items_path
   end
 
   private
