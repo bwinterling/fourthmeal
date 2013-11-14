18e11cab8fbb6a5546a437a0fd954435043c2e9a
Ben Lewis <bennlewis@gmail.com>
2013-11-13 19:06:23 -0700

    cleaned up lines in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 OnoBurrito::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  root :to => "items#index"
+
+  resources :contacts
+  resources :items
+  resources :locations
+  resources :orders  
+  resources :order_items
+  resources :sessions
+  resources :transactions
+  resources :users
+
   get "log_out" => "sessions#destroy"
   get "log_in" => "sessions#new"
-  get "sign_up" => "users#new"
-  root :to => "items#index"
-  resources :users
-  resources :sessions
-
-  resources :items do
-    resources :item_categories
-  end
   get 'menu' => 'items#index', as: :menu
-
   get "menu/:category_slug" => "items#in_category", as: "menu_items"
-  resources :orders  
-  resources :contacts
-  resources :order_items
-  resources :locations
-  resources :transactions
+  get "sign_up" => "users#new"
+
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
 end
