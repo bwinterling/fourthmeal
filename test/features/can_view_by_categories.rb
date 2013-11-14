3e39bac6233caf5e3fcbd7541e7206d2fd5e5e57
Ben Lewis <bennlewis@gmail.com>
2013-11-14 08:31:32 -0700

    uncommented view by category test

--- test/features/can_view_by_categories.rb
+++ test/features/can_view_by_categories.rb
@@ -1,15 +1,15 @@
-# require './test/test_helper'
+require './test/test_helper'
 
-# class CanViewByCategoriesTest < Capybara::Rails::TestCase
+class CanViewByCategoriesTest < Capybara::Rails::TestCase
 
-# test "can see all items by category" do
-#     categories = Category.create(name: "Kids Menu")
-#     item = Item.create(title: 'Kids Taco', description: 'Yummy', price: '1')
-#     category.items << item
+test "can see all items by category" do
+    categories = Category.create(name: "Slushees")
+    item = Item.create(title: 'Cherry Slushee', description: 'Yummy', price: '1')
+    category.items << item
 
-#     visit root_path
-#     assert_content page, "Burrito"
-#     assert_content page, "Kids Menu"
-#   end
+    visit root_path
+    assert_content page, "Cherry Slushee"
+    assert_content page, "Slushees"
+  end
 
-# end
+end
