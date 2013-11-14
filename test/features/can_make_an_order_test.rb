7a34dbb15b9bb18adc708ed47254efed2e160f7c
Ben Lewis <bennlewis@gmail.com>
2013-11-14 08:41:40 -0700

    completed make an order test

--- test/features/can_make_an_order_test.rb
+++ test/features/can_make_an_order_test.rb
@@ -4,55 +4,40 @@ require './test/test_helper'
 class CanMakeAnOrderTest < Capybara::Rails::TestCase
 
   test "a user can create an order" do
+    item = Item.create(title: 'Steak Burrito', description: 'Mouthwatering slab', price: '1')
 
     visit root_path
 
-    within "#item_1" do
+    within "#item_#{item.id}" do
       click_on "Add to Cart"
     end
 
     assert_content page, 'Your Current Order'
-
     assert_content page, "Mouthwatering slab"
     
-    within "#item_1" do
-      click_on "Add to Cart"
-    end   
-
   end
-
-#   test "can add an item to the current order" do
-#     item = Item.new(title: 'Burrito', description: 'Yummy', price: '1')
-#     item.save
-#     visit root_path
-#     within("#orders") do
-#       click_on "Add to Order"
-#     end
-
-#     assert_content page, "Burrito added to cart!"
-#   end
 
   
 
-#   test "can add multiple items to order without logging in" do
-#     Item.create(title: 'Burrito', description: 'Yummy', price: '1')
-#     Item.create(title: 'Nachos', description: 'Delicious', price: '1')
-#     visit root_path
-#     within("#order") do
-#       click_on "Add to Cart"
-#     end
-#     within("#item_2") do
-#       click_on "Add to Cart"
-#     end
+  test "can add multiple items to order without logging in" do
+    item1 = Item.create(title: 'Steak Burrito', description: 'Mouthwatering slab', price: '1')
+    item2 = Item.create(title: 'Breakfast Burrito', description: 'Yummy', price: '1')
 
-#     visit order_path(Order.first)
-#     within("#item_1") do
-#       assert_content page, "Burrito"
-#     end
-#     within("#item_2") do
-#       assert_content page, "Nachos"
-#     end
-#   end
+    visit root_path
+
+    within "#item_#{item1.id}" do
+      click_on "Add to Cart"
+    end
+
+    within "#item_#{item2.id}" do
+      click_on "Add to Cart"
+    end
+
+    within("#current_order") do
+      assert_content page, "Breakfast Burrito"
+      assert_content page, "Steak Burrito"
+    end
+  end
 
 #   test "can add multiple instances of same item to order" do
 #     Item.create(title: 'Burrito', description: 'Yummy', price: '1')
