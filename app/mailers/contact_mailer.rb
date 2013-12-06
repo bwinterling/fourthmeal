f8b3e620d80b90a6e007701b94e1f8bfb5a0d8b4
Brian Winterling <bwinterling@yahoo.com>
2013-12-06 13:50:46 -0700

    added account confirmation email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,11 @@ class ContactMailer < ActionMailer::Base
 
   def contact_requested(contact)
     @contact = contact
-
     mail to: "bwinterling@yahoo.com", subject: contact.subject
+  end
+
+  def account_confirmation(user)
+    @user = user
+    mail to: user.email, subject: "Thank you for creating an account!"
   end
 end
