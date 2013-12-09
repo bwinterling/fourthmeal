581ff61876dc2ddfb433b1519ef606b36b899f9e
Brian Winterling <bwinterling@yahoo.com>
2013-12-09 14:51:52 -0700

    fixed error with Devise migration

--- db/migrate/20131113004337_devise_create_admin_users.rb
+++ db/migrate/20131113004337_devise_create_admin_users.rb
@@ -1,8 +1,8 @@
 class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
-    # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
+    # # Create a default user
+    # AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password') if direction == :up
   end
 
   def change
